581effdacde4ac95d3868830cb6cb7b4151cf8f4
jihyun <lifeisggv@gmail.com>
2025-04-13 16:38:25 +0900

    test1

--- jihyun/src/textbook/Week01_Q1.java
+++
@@ -1,7 +0,0 @@
-package textbook;
-
-public class Week01_Q1 {
-    public static void main(String[] args) {
-        System.out.println("문제 1번 풀이 시작!");
-    }
-}
---
+++ jihyun/src/textbook/Week01_p41_02.java
@@ -0,0 +1,10 @@
+package textbook;
+public class Week01_p41_02 {
+    public static void main(String[] args) {
+    
+    
+    
+    
+        System.out.println("🧪 " + Week01_p41_02.class.getSimpleName() + " 실행!");
+    }
+}
