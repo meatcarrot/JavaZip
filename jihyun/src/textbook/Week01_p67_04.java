a6a8da17174fa22d6d4474aabc83b74b2e934b6b
jihyun <lifeisggv@gmail.com>
2025-04-15 22:56:26 +0900

    [BOOK] p68_09

---
+++ jihyun/src/textbook/Week01_p67_04.java
@@ -0,0 +1,16 @@
+package textbook;
+
+public class Week01_p67_04 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 2.0;
+
+        // 사칙연산의 결과를 정수로 출력
+        System.out.println("a + b = " + (int)(a+b));
+        System.out.println("a - b = " + (int)(a-b));
+        System.out.println("a x b = " + (int)(a*b));
+        System.out.println("a % b = " + (int)(a/b));
+
+        System.out.println("🧪 " + Week01_p67_04.class.getSimpleName() + " 실행!");
+    }
+}
---
+++ jihyun/src/textbook/Week01_p67_05.java
@@ -0,0 +1,13 @@
+package textbook;
+
+public class Week01_p67_05 {
+    public static void main(String[] args) {
+
+        char ch = '글';
+
+        System.out.println(ch);
+        System.out.println((int)ch);
+
+        System.out.println("🧪 " + Week01_p67_05.class.getSimpleName() + " 실행!");
+    }
+}
