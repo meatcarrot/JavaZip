a43a2b1077ecb8b3ca9de3bace444207f3e3b3c2
jihyun <lifeisggv@gmail.com>
2025-04-15 23:34:52 +0900

    [BOOK] p67_05

--- jihyun/src/textbook/Week01_p67_05.java
+++ jihyun/src/textbook/Week01_p67_05.java
@@ -4,9 +4,11 @@ public class Week01_p67_05 {
     public static void main(String[] args) {
 
         char ch = '글';
+        char ch2 = '\uAE00';
 
         System.out.println(ch);
-        System.out.println((int)ch);
+        System.out.println((int)ch); // 정수형
+        System.out.println(ch2);
 
         System.out.println("🧪 " + Week01_p67_05.class.getSimpleName() + " 실행!");
     }
