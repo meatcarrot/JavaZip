3151450e12cd6614c4fd1616b2baf74b2e5b5628
jihyun <lifeisggv@gmail.com>
2025-04-16 18:29:29 +0900

    [CDM]비밀번호_찾기

--- jihyun/src/personal_practice/CDM_비밀번호_찾기.java
+++ jihyun/src/personal_practice/CDM_비밀번호_찾기.java
@@ -1,4 +1,20 @@
 package personal_practice;
 
+import java.util.*;
+
 public class CDM_비밀번호_찾기 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String insert = sc.nextLine();
+        String[] arr = insert.split(" ");
+
+        for (String ch : arr){
+            System.out.print(ch + " ");
+            if (ch.equals("c")) break;
+        }
+        System.out.println();
+        System.out.println("🧪 " + CDM_비밀번호_찾기.class.getSimpleName() + " 실행!");
+    }
 }
