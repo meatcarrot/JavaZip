8e38ea441c4193d13b205ca6e0f5d2a0484efad5
Jun-gi Kim <damnum3@hanyang.ac.kr>
2025-04-30 03:19:56 +0900

    [CDM] 중급_치팅 검사

---
+++ Joon-Gi/CDM/Intermediate/src/CDM_치팅검사.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class CDM_치팅검사 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        int n = input.length();
+        boolean isCheated = false;
+        String answer = "";
+
+        for(int i=1; i<=n/2; i++) {
+            String firstPart = input.substring(0, i);
+            String firstPartTarget = input.substring(i, 2*i);
+
+            String secondPart = input.substring(2*i, 2*i+(n/2-i));
+            String secondPartTarget = input.substring(2*i+(n/2-i), n);
+
+            if(firstPart.equals(firstPartTarget) && secondPart.equals(secondPartTarget)) {
+                answer = input.substring(i, 2*i+(n/2-i));
+                isCheated = true;
+
+                break;
+            }
+        }
+
+        if(isCheated) {
+            System.out.println("YES");
+            System.out.print(answer);
+        } else {
+            System.out.print("NO");
+        }
+    }
+}
