c84200b8fd031ce76a52be429e9d597397ac2ecb
jx-hxxx <lifeisggv@gmail.com>
2025-05-14 20:45:47 +0900

    Delete Jiwon directory

--- Jiwon/Week01_chapter1/src/chapter1_practice/Week01_p36_07.java
+++
@@ -1,8 +0,0 @@
-package chapter1_practice;
-
-public class chapter1_practice7 {
-    public static void main(String[] args){
-        // 1장 되새김 문제 7번
-        System.out.println("자바 프로그래밍 재미있다!");
-    }
-}
