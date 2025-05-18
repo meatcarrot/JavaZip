28ef09a80187bf1dd2225f4746bce1fdca6631f9
Jun-gi Kim <damnum3@hanyang.ac.kr>
2025-05-18 23:51:02 +0900

    [BOOK] Week06_p465_08

---
+++ Joon-Gi/BOOK/Chapter12/src/collection/arraylist/Week06_p465_08.java
@@ -0,0 +1,24 @@
+package collection.arraylist;
+
+import collection.Member;
+
+public class Week06_p465_08 {
+    public static void main(String[] args) {
+        MemberArrayList memberArrayList = new MemberArrayList();
+
+        Member memberLee = new Member(1001, "이지원");
+        Member memberSon = new Member(1002, "손민국");
+        Member memberPark = new Member(1003, "박서훤");
+        Member memberHong = new Member(1004, "홍길동");
+
+        memberArrayList.addMember(memberLee);
+        memberArrayList.addMember(memberSon);
+        memberArrayList.insertMember(1, memberPark);
+        memberArrayList.insertMember(3, memberHong);
+
+        memberArrayList.showAllMember();
+
+        memberArrayList.removeMember(memberHong.getMemberId());
+        memberArrayList.showAllMember();
+    }
+}
