04dd0657e191f5329c3981b7c734f9ecb17fdb05
Jun-gi Kim <damnum3@hanyang.ac.kr>
2025-05-19 22:48:12 +0900

    [BOOK] chapter13

---
+++ Joon-Gi/BOOK/Chapter13/src/optional/UserInfo.java
@@ -0,0 +1,19 @@
+package optional;
+
+public class UserInfo {
+    private int id;
+    private String name;
+
+    public UserInfo(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Joon-Gi/BOOK/Chapter13/src/stream/LimitTest.java
@@ -0,0 +1,21 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LimitTest {
+    public static void main(String[] args) {
+        List<String> fruits = Arrays.asList("파인애플", "사과", "바나나", "귤");
+
+        System.out.print("기본 정렬(오름차순): ");
+        fruits.stream()
+                .sorted().limit(2)
+                .forEach(s -> System.out.print(s + " "));
+        System.out.println();
+
+        System.out.print("맞춤형 정렬(문자열 길이): ");
+        fruits.stream()
+                .sorted((f1, f2) -> Integer.compare(f1.length(), f2.length())).limit(2)
+                .forEach(s -> System.out.print(s + " "));
+    }
+}
